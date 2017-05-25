d48aef94dfc335568f2a967508b4abfd9059731c
Yasmin Alves <yasminleticia_@hotmail.com>
2017-05-24 21:08:54 -0300

    Revert "oii"
    This reverts commit 8d0dd709ba87e4926c54933be794c0d29727de51.

--- src/br/ufrpe/negocio/beans/Task.java
+++ src/br/ufrpe/negocio/beans/Task.java
@@ -1,17 +1,15 @@
 package br.ufrpe.negocio.beans;
 
 public class Task {
-	
-	//oi
 
 	private String data;
 	private String nome;
 	private String descricao;
 	private String tipo;
 	private String disciplina;
-	private Task lembrar;
 
 
+	
 	
 	public Task() {
 		
@@ -26,16 +24,6 @@ public class Task {
 		this.disciplina = disciplina;
 	}
 	
-	
-	
-	public Task getLembrar() {
-		return lembrar;
-	}
-
-	public void setLembrar(Task lembrar) {
-		this.lembrar = lembrar;
-	}
-
 	public String getData() {
 		return data;
 	}
@@ -65,14 +53,6 @@ public class Task {
 	}
 	public void setDisciplina(String disciplina) {
 		this.disciplina = disciplina;
-	}
-	
-	public void deleteTask(Task lemb){
-		lemb.data = null;
-		lemb.descricao = null;
-		lemb.disciplina = null;
-		lemb.nome = null;
-		lemb.tipo = null;
 	}
 
 	
