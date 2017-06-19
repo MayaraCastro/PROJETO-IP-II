b4f3fdfd1239e2f5fd672e9b480120aedbe51980
Yasmin ALves <yasminleticia_@hotmail.com>
2017-06-18 22:14:35 -0300

    Update CadastroDisciplina.java
    mudança em cadastrar

--- src/br/ufrpe/my_pigeon_study/negocio/CadastroDisciplina.java
+++ src/br/ufrpe/my_pigeon_study/negocio/CadastroDisciplina.java
@@ -7,24 +7,42 @@ public class CadastroDisciplina {
 	}
 	public boolean cadastrar(Usuario user,Disciplina disci){
 
-			if(disci.getNome()!="" && disci.getDia_da_semana()!= 0 
-			&& disci.getHorario()!= null && disci.getProfessor()!= ""
-			&& disci.getObs()!= "" && disci.getPeriodo()!= null )
-			{
-				if(user.addDisciplina(disci)){
+			public boolean cadastrar(Usuario user,Disciplina disci){
+
+			if(disci.getNome()!="" ){
+				
+				if(rep.inserir(disci)){
 					return(true);	
 				}
 			}
 			return(false);
 		
 	}
+		
+	}
 	public Disciplina buscar(Usuario user, String nome){
-		return(user.getCalendario().getDisciplinas().buscar(nome));
+		return();
 	}
 	public boolean alterar(Usuario user,Disciplina disci, String original){
-		return(user.getCalendario().getDisciplinas().alterar(disci,original));
+		if(disci.getNome() != "" && disci.getDia_da_semana() != 0 && disci.getHorario() != null && disci.getProfessor() != "" 
+				&& disci.getObs() != "" & disci.getPeriodo() != ""){
+						Disciplina novo = this.rep.buscar(); //TODO bug
+						novo.setNome(disci.getNome());
+						novo.setDia_da_semana(disci.getDia_da_semana());
+						novo.setHorario(disci.getHorario());
+						novo.setProfessor(disci.getProfessor());
+						novo.setObs(disci.getObs());
+						novo.setPeriodo(disci.getPeriodo());
+						
+						if(this.rep.alterar(novo)){
+							return(true);
+						}
+				}
+				return(false);
+	
+		
 	}
 	public boolean remover(Usuario user, String disci){
-		return(user.getCalendario().getDisciplinas().remover(disci));
+		return()); 
 	}
 }
